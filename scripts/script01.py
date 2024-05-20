eff18fde61f56b35286e431301adfc71ca3aca4e
Julio Neri <160162554+julioneri@users.noreply.github.com>
2024-05-20 00:34:41 -0300

    Update script01.py

--- scripts/script01.py
+++ scripts/script01.py
@@ -1,3 +1,53 @@
+"""
+Desafio: Criando um Sistema de Gestão de Clientes
+
+Crie um programa em JavaScript que simule um sistema de gestão de clientes para uma empresa. Cada cliente terá as seguintes propriedades:
+
+1. Nome
+2. E-mail
+3. Telefone
+
+O programa deve ser capaz de realizar as seguintes operações:
+
+1. Adicionar um novo cliente ao sistema.
+2. Listar todos os clientes cadastrados.
+3. Buscar um cliente pelo nome ou e-mail.
+4. Remover um cliente do sistema.
+"""
+
+lista_clientes = []
+
+
+def text_header(txt='Insert Your Text Here'):  # Cria um cabeçalho com o texto inserido
+    print('\n' + f' {txt} '.center(50, '='))
+    return
+
+
+def adicionar_cliente():
+    nome_cliente = str(input('Digite o nome do cliente: ')).strip().title()
+    email_cliente = str(input('Digite o E-mail para contado: ')).strip().lower()
+    telefone_cliente = int(input('Digite o telefone para contato: '))
+
+    cliente = {
+        'nome': nome_cliente,
+        'email': email_cliente,
+        'telefone': telefone_cliente
+        }
+
+    lista_clientes.append(cliente.copy())
+
+    print('\n\033[32mCliente registrado com sucesso!\033[m')
+    return
+
+
+def listar_clientes():
+    for c, cliente in enumerate(lista_clientes):
+        print(f'Nome: {cliente['nome']}')
+        print(f'E-mail: {cliente['email']}')
+        print(f'Telefone: {cliente['telefone']}')
+        return
+
+
 while True:
     text_header('Gestor de Clientes')
     print('1. Adicionar um novo cliente ao sistema.'
